e37dbd40830aac6ad39c364cf87e8021cf02090d
skyoo1226 <you1226@naver.com>
2022-03-05 22:37:14 +0900

    onCreate에 setupEvents()와 setValues()를 넣어서 쓸것 임.

--- app/src/main/java/com/skyoo/lifecycle_20220305_exam/MainActivity.kt
+++ app/src/main/java/com/skyoo/lifecycle_20220305_exam/MainActivity.kt
@@ -2,10 +2,29 @@ package com.skyoo.lifecycle_20220305_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+        btnMove.setOnClickListener {
+
+
+        }
+
+    }
+
+    fun setValues() {
+
+        Log.d("메인화면", "onCreate 실행됨")
+    }
+
 }
